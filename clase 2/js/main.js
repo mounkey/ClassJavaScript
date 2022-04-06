06c14f6abb1606f515f4ce9238ffecb9c5ff5567
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-04-06 15:57:45 -0400

    tarea clase 2

---
+++ clase 2/js/main.js
@@ -0,0 +1,11 @@
+alert("Hola!!");
+let nombre = prompt(" Pon tu nombre. por favor ");
+let edad =  prompt(" Coloque su edad ");
+let pass = prompt(" Coloque su password ");
+document.write(pass);
+if (pass == "Abril2022"){
+  alert("Bienvenido " +  nombre);
+}
+else{
+ alert("No puede entrar, Gracias ")
+}
