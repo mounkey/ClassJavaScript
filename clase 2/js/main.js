6b755598c517778b59aaf6709727ba7d312109c4
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-04-09 18:28:10 -0400

    ejercicio de emergencia

--- clase 2/js/main.js
+++ clase 2/js/main.js
@@ -1,13 +1,13 @@
 const pass = "Abril2020";
 alert("Hola Buenos Dias:");
-let name = prompt("Ingrese su nombre:");
+let nombre = prompt("Ingrese su nombre:");
 let passUser = prompt("Ingrese la pass");
 if (passUser === pass){
-  aler("Puede Ingtresar a su cuenta, estimado " +  name + ".");
+  aler("Puede Ingtresar a su cuenta, estimado " + nombre + ".");
   
 }
 else{
-  alert("No puede ingresar " + name + ".\n Gracias");
+  alert("No puede ingresar " + nombre + ".\n Gracias");
 }
 
 document.write(" Que tengas un excelente dia ");
---
+++ clase 2/js/main2.js
@@ -0,0 +1,12 @@
+const NUM = 1000
+let numero = Number(prompt("Agregue su numero"));
+if ( numero > NUM){
+    alert ("numero mayor a 1000");
+}
+    else if (numero < 1000){
+        alert("numero menor a 1000");
+    
+}
+ else{
+     alert("el numero es igual")
+ }
