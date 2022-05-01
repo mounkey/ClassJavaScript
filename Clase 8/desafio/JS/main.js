ed8dd4112edb2eddd1b59b9edf7a75d8531fb8c1
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-05-01 17:48:58 -0400

    Desafio 8

---
+++ Clase 8/desafio/JS/main.js
@@ -0,0 +1,32 @@
+/*
+let boton =document.querySelector("button");
+let body = document.querySelector("body");
+let cambiarFondo = function(){
+  body.style.backgroundColor="#AAFFBB";
+}
+boton.addEventListener("click",cambiarFondo);  */
+
+let body = document.getElementsByTagName("body")[0];
+let Lector = document.getElementById("lector");
+let Resp = document.getElementById("respuesta");
+let p = document.createElement("p")
+let Persona;
+
+function changeColor(){
+  body.style.background='#000000';
+}
+
+function addText(){
+  Lector.innerHTML = "<p >Hola como estas??</p> <br> <button onclick= 'btnRespuesta()'>Respuesta</button> <br>";
+  Lector.innerHTML += "<input type ='text'> ";
+  lector.style.color= "yellow";
+  Lector.style.fontSize= "2rem";
+}
+
+function btnRespuesta(){
+  Persona = document.getElementsByTagName("input")[0];
+  Resp.textContent= `Hola espero que estes bien ${Persona.value}`;
+  Resp.style.color = "blue";
+  Resp.style.fontSize = "3rem";
+}
+
